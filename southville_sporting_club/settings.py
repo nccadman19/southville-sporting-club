7f3e07259fea629a180341813f59e8730b518d07
Nic <nicolecadman@icloud.com>
2023-08-13 16:18:44 +0000

    Fixed static files paths and references for proper loading of CSS and JS

--- southville_sporting_club/settings.py
+++ southville_sporting_club/settings.py
@@ -150,6 +150,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'extra_static'),  # Include the 'extra_static' directory
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
