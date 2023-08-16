7bd5a9e02b266efa2daefaa6be1a60affe7b308f
Nic <nicolecadman@icloud.com>
2023-08-16 19:48:25 +0000

    Implemented top banner design, added test content and hero image for Django functionality, and linked static files to HTML pages

--- southville_sporting_club/settings.py
+++ southville_sporting_club/settings.py
@@ -148,11 +148,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'extra_static'),  # Include the 'extra_static' directory
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
