02395f144e1d4023afe080a35e34c46c8643381f
Nic <nicolecadman@icloud.com>
2023-08-20 17:31:13 +0000

    Refactored navigation to separate HTML page, added search close button

--- extra_static/js/init.js
+++ extra_static/js/init.js
@@ -8,4 +8,18 @@ document.addEventListener('DOMContentLoaded', function () {
   var dropdownInstances = M.Dropdown.init(dropdownElems, dropdownOptions);
   var elems = document.querySelectorAll('.collapsible');
   var instances = M.Collapsible.init(elems, { accordion: false });
+  var searchInput = document.getElementById("search");
+  var closeIcon = document.querySelector(".close-icon");
+
+  // When the search input gains focus (is clicked)
+  searchInput.addEventListener("focus", function () {
+    // Show the close icon
+    closeIcon.style.display = "inline";
+  });
+
+  // When the search input loses focus (is clicked outside of)
+  searchInput.addEventListener("blur", function () {
+    // Hide the close icon
+    closeIcon.style.display = "none";
+  });
 });
