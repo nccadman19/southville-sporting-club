95d465b361877523ec10dcdcc782d87acf3fc292
Nic <nicolecadman@icloud.com>
2023-08-23 15:54:10 +0000

    Customised admin panels

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
 from .models import Product, Category
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+# Models
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'price',
+        'category',
+        'updated_at'
+    )
+    list_filter = (
+        'category',
+        'updated_at'
+    )
+    search_fields = ('sku', 'name')
+    list_editable = ('price',)
+    list_per_page = 20
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+        'updated_at')
+    search_fields = ('name', 'friendly_name')
+    prepopulated_fields = {'friendly_name': ('name',)}
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=100, unique=True)
     friendly_name = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
