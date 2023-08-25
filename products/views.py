90c0efe59a63e6221755273e86d6c0c796f020f2
Nic <nicolecadman@icloud.com>
2023-08-25 11:45:55 +0000

    Moved searchbar on mobile and added search functionality

--- products/views.py
+++ products/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 def product_list(request):
     # Retrieve all products from the database
     products = Product.objects.all()
-    context = {'products': products, 'title': 'Product List'}
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+            if not products:
+                messages.error(request, "No products match your search criteria.")
+                return redirect(reverse('products'))
+
+    context = {
+        'products': products,
+        'title': 'Product List',
+        'search_term': query,
+    }
     return render(request, 'products/products.html', context)
 
 def product_detail(request, product_id):
