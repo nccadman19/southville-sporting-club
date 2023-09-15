31e86b76795ba501904bf34ac474bdab9d92247a
Nic <nicolecadman@icloud.com>
2023-09-15 21:23:28 +0000

    Styled cart, added size functionality to details

---
+++ products/static/products/js/product_detail.js
@@ -0,0 +1,65 @@
+// Define the validateForm function in the global
+function validateForm() {
+    var selectedSize = document.getElementById("selected-size").value;
+    if (selectedSize === "") {
+        alert("Please select a size.");
+        return false;
+    }
+    return true;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Get all the size buttons within the product detail section
+    var sizeButtons = document.querySelectorAll('.product-detail .product-size-button');
+
+    // Initialise a variable to store the selected size
+    var selectedSize = null;
+
+    // Attach a click event listener to each size button
+    sizeButtons.forEach(function (button) {
+        button.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            // Deselect all buttons within the product detail section
+            sizeButtons.forEach(function (btn) {
+                btn.classList.remove('selected');
+            });
+
+            // Select the clicked button
+            button.classList.add('selected');
+
+            // Store the selected size in the variable
+            selectedSize = button.dataset.size;
+
+            // Update the hidden input field immediately
+            document.getElementById("selected-size").value = selectedSize;
+        });
+    });
+
+    // Get the "Add to Bag" form
+    var addToBagForm = document.querySelector('.form');
+
+    // Attach a submit event listener to the "Add to Bag" form
+    addToBagForm.addEventListener('submit', function (e) {
+        // Validate the selected size
+        var selectedSize = document.getElementById("selected-size").value;
+        if (selectedSize === "") {
+            e.preventDefault(); // Prevent form submission
+            alert("Please select a size.");
+        }
+    });
+
+    // When the product details cards clicked shows respective icon
+    var collapsibleHeaders = document.querySelectorAll('.collapsible-header-product');
+
+    collapsibleHeaders.forEach(function (header) {
+        header.addEventListener('click', function () {
+            var icon = header.querySelector('.icon');
+            if (icon.innerHTML === '+') {
+                icon.innerHTML = '-';
+            } else {
+                icon.innerHTML = '+';
+            }
+        });
+    });
+});
