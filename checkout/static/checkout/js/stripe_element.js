900bfde1e75a43f5c9f4c43f8f736ced3b1844f9
Nic <nicolecadman@icloud.com>
2023-09-21 23:28:52 +0000

    started setting up tokens, redundant code removed

--- checkout/static/checkout/js/stripe_element.js
+++ checkout/static/checkout/js/stripe_element.js
@@ -14,12 +14,25 @@ form.addEventListener('submit', function (event) {
     event.preventDefault();
 
     // Use Stripe.js to create a token and handle the payment
-    stripe.createToken(card).then(function (result) {
+    stripe.createToken(card, {
+        name: document.getElementById('id_full_name').value,
+        email: document.getElementById('id_email').value,
+        // phone: document.getElementById('id_phone_number').value,
+        address_line1: document.getElementById('id_street_address1').value,
+        address_line2: document.getElementById('id_street_address2').value,
+        address_city: document.getElementById('id_town_or_city').value,
+        address_zip: document.getElementById('id_postcode').value,
+        address_country: document.getElementById('id_country').value,
+    }).then(function (result) {
         if (result.error) {
             // Show error to your customer
             console.error(result.error.message);
         } else {
-            // Find out how to add the code to link databases
+            // Token created, you can now submit your form with the token ID
+            // and handle the server-side processing.
+            var token = result.token;
+            // Send the token to your server for processing.
+            // Handle the server-side logic to complete the payment.
         }
     });
 });
