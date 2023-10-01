0e5126a49cee97d2640957e9ce216d6833105ab3
Nic <nicolecadman@icloud.com>
2023-10-01 20:06:33 +0000

    secret key moved to env file

--- southville_sporting_club/settings.py
+++ southville_sporting_club/settings.py
@@ -18,12 +18,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4c18glil@s7e!0(v#b2sy$qqs#o#lpfm1@9)q6l$(n#ttlzjjb'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
