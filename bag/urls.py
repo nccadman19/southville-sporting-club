ab1758e5cd883ab096a7449fb793f239eac92b8b
Nic <nicolecadman@icloud.com>
2023-10-03 19:57:04 +0000

    Remove item created for bag

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('remove/<int:item_id>/', views.remove_item, name='remove_item'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.http import JsonResponse
 from django.contrib import messages
 from . import views
 from products.models import Product
@@ -29,4 +30,30 @@ def add_to_bag(request, item_id):
     messages.success(request, message)
 
     request.session['bag'] = bag
+    request.session.modified = True  """ Indicate that the session has been modified """
     return redirect(redirect_url)
+
+def remove_item(request, item_id):
+    selected_size = request.POST.get('selected_size')
+
+    """ Update the cart in the session """
+    bag = request.session.get('bag', {})
+    item_id = f"{item_id}_{selected_size}"
+
+    """ Remove one item from bag """
+    if item_id in bag:
+        if bag[item_id] > 1:
+            bag[item_id] -= 1  
+        else:
+            del bag[item_id]  
+        request.session.modified = True
+
+        """ Send a success message to the user """
+        messages.success(request, f'Removed one item from your bag')
+
+        return JsonResponse({'status': 'success'})
+    else:
+        """ If the item was not found in the bag, send an error message """
+        return JsonResponse({'status': 'error', 'message': 'Item not found in your bag'})
+
+
