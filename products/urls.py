912fde400cf0ed9746d6a63aeb01eb3943c4028b
Nic <nicolecadman@icloud.com>
2023-10-19 20:02:50 +0000

    Edit product admin template, url and view started

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.product_list, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('nothing-found/', views.nothing_found, name='nothing_found'),
 ]
