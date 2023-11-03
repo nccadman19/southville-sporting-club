42e4354da204ac92bf3c47e26584caa078ada5f1
Nic <nicolecadman@icloud.com>
2023-11-03 21:31:33 +0000

    user profile added to checkout admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,22 +12,18 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total')
 
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
-                    'grand_total_with_delivery',)
+                    'grand_total',)
 
     ordering = ('-date',)
-
-    def grand_total_with_delivery(self, obj):
-        return obj.grand_total
-    grand_total_with_delivery.short_description = 'Grand Total with Delivery'
 
 admin.site.register(Order, OrderAdmin)
