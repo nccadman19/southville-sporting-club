2fb82027762f331b74a4cd73f770a2326ea90b00
Nic <nicolecadman@icloud.com>
2023-11-06 22:42:24 +0000

    Order list planned, noted error with product size

--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -1,14 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
-from products.models import Product
 from checkout.models import Order
 
 @login_required
 def order_list(request):
     if request.user.is_staff:
-        # User is an admin, so allow access to the view
-        return render(request, 'order_list.html')
+        orders = Order.objects.all().prefetch_related('lineitems')
+        return render(request, 'admin_dashboard/order_list.html', {'orders': orders})
     else:
         # User is not an admin, return a 404 Not Found response
         return HttpResponseNotFound()
