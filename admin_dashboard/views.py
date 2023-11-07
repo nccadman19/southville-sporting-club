685f5fbf0ca3dcf5292a7e8504447e60a00ebad4
Nic <nicolecadman@icloud.com>
2023-11-07 01:22:38 +0000

    Stock list template created for site owner

--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from checkout.models import Order
+from products.models import Product
 
 @login_required
 def order_list(request):
@@ -14,5 +15,5 @@ def order_list(request):
 
 @login_required
 def stock_list(request):
-    # Your view logic here
-    return render(request, 'admin_dashboard/stock_list.html')
+    products = Product.objects.all()
+    return render(request, 'admin_dashboard/stock_list.html', {'products': products})
