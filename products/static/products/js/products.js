1e30fc6a6db5383a14c012e863c834295fee2f34
Nic <nicolecadman@icloud.com>
2023-11-08 20:20:44 +0000

    Moved the 'back to top' button code to a common layout for availability on all pages.

--- products/static/products/js/products.js
+++ products/static/products/js/products.js
@@ -1,33 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get the elements related to the scroll-to-top button if they exist
-    var backToTopBtn = document.getElementById('back-to-top');
-    var backToTopContainer = document.getElementById('back-to-top-container');
-
     // Initialise the specific dropdown trigger with click option and coverTrigger
     var sortDropdownElem = document.querySelector('.sort-dropdown-trigger');
     var dropdownOptions = { alignment: 'right', coverTrigger: false, closeOnClick: true };
     var specificDropdownInstance = M.Dropdown.init(sortDropdownElem, dropdownOptions);
-
-    // Attach the scroll-to-top button functionality if the elements exist
-    if (backToTopBtn && backToTopContainer) {
-        function toggleBackToTopButton() {
-            if (window.scrollY > 300) {
-                backToTopContainer.style.display = 'block';
-            } else {
-                backToTopContainer.style.display = 'none';
-            }
-        }
-
-        window.addEventListener('scroll', toggleBackToTopButton);
-
-        backToTopBtn.addEventListener('click', function () {
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            });
-            backToTopBtn.classList.remove('waves-ripple');
-        });
-    }
 
     // Add a delete item popup modal 
     var modals = document.querySelectorAll('.modal');
--- static/js/init.js
+++ static/js/init.js
@@ -105,4 +105,29 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
   }
+
+  // Get the elements related to the scroll-to-top button if they exist
+  var backToTopBtn = document.getElementById('back-to-top');
+  var backToTopContainer = document.getElementById('back-to-top-container');
+
+  // Attach the scroll-to-top button functionality if the elements exist
+  if (backToTopBtn && backToTopContainer) {
+    function toggleBackToTopButton() {
+      if (window.scrollY > 300) {
+        backToTopContainer.style.display = 'block';
+      } else {
+        backToTopContainer.style.display = 'none';
+      }
+    }
+
+    window.addEventListener('scroll', toggleBackToTopButton);
+
+    backToTopBtn.addEventListener('click', function () {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+      backToTopBtn.classList.remove('waves-ripple');
+    });
+  }
 });
