71125730412bbcee76fa66c3520adbd1ee003838
Nic <nicolecadman@icloud.com>
2023-11-10 20:01:27 +0000

    Deployment started, elephant sql connected

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn southville-sporting-club.wsgi:application
--- southville_sporting_club/settings.py
+++ southville_sporting_club/settings.py
@@ -16,6 +16,7 @@ from decouple import config
 from dotenv import load_dotenv
 
 import os
+import dj_database_url
 
 
 load_dotenv()
@@ -29,9 +30,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '8000-nccadman19-southvillesp-dw34vuby9zs.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['localhost', 'southville-sporting-club-ed7d11c9dd6e.herokuapp.com']
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://127.0.0.1', 'https://8000-nccadman19-southvillesp-dw34vuby9zs.ws-eu106.gitpod.io/*']
+CSRF_TRUSTED_ORIGINS = ['http://localhost', 'https://southville-sporting-club-ed7d11c9dd6e.herokuapp.com/*']
 
 # Application definition
 
@@ -122,12 +123,17 @@ WSGI_APPLICATION = 'southville_sporting_club.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 STRIPE_PUBLIC_KEY = config('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
