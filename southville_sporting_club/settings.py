d11897271f78775c88a1ca853e09a5f6790e7c20
Nic <nicolecadman@icloud.com>
2023-11-11 11:18:19 +0000

    Implementation of email started

--- southville_sporting_club/settings.py
+++ southville_sporting_club/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.apple',
     'allauth.socialaccount.providers.google',
     'home',
     'products',
@@ -102,8 +101,6 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SITE_ID = 2
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -226,11 +223,17 @@ DELIVERY_THRESHOLDS = {
     'UK_MAINLAND_1ST_CLASS': Decimal('150.00'),
 }
 
-# Stripe urls and domains
-# BACKEND_DOMAIN = config("BACKEND_DOMAIN")
-
-# Email
-DEFAULT_FROM_EMAIL = 'noreply@southville-sporting-club.com'
-
 # Custom 404 page
 handler404 = 'home.views.error'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'noreply@southville-sporting-club.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
