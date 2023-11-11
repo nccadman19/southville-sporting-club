7521556b963c104c487a00ef068be3b2b068b035
Nic <nicolecadman@icloud.com>
2023-11-11 14:19:14 +0000

    Updated the checkout_success view to handle saving user profile information test 1

--- checkout/static/checkout/js/checkout.js
+++ checkout/static/checkout/js/checkout.js
@@ -36,12 +36,48 @@ document.addEventListener('DOMContentLoaded', function () {
         $('#grand-total-input').val(newGrandTotal.toFixed(2));
     }
 
+    // Trigger the initial update when the page loads
+    updateHiddenDeliveryCost(selectedDeliveryType);
+
+    var saveInfoCheckbox = document.getElementById('id-save-info');
+
+    saveInfoCheckbox.addEventListener('change', function () {
+        if (!saveInfoCheckbox.checked) {
+            saveInfoCheckbox.removeAttribute('checked');
+        }
+    });
+
     // Listen for changes in the delivery type radio buttons
     $('input[name="delivery_type"]').on('change', function () {
         var selectedDeliveryType = $(this);
         updateHiddenDeliveryCost(selectedDeliveryType);
     });
 
-    // Trigger the initial update when the page loads
-    updateHiddenDeliveryCost(selectedDeliveryType);
+    // Function to update the user's profile information via AJAX
+    function updateProfileInfo() {
+        // Collect the user's information from the form
+        var formData = {
+            // Extract other form fields as needed
+            'default_phone_number': $('#id_default_phone_number').val(),
+            'default_country': $('#id_default_country').val(),
+            'default_postcode': $('#id_default_postcode').val(),
+            'default_town_or_city': $('#id_default_town_or_city').val(),
+            'default_street_address1': $('#id_default_street_address1').val(),
+            'default_street_address2': $('#id_default_street_address2').val(),
+            'default_county': $('#id_default_county').val(),
+        };
+
+        // Send the data to the server using AJAX
+        $.ajax({
+            type: 'POST',
+            url: '/update_profile/',  // Replace with your actual URL for updating the profile
+            data: formData,
+            success: function (data) {
+                console.log('Profile information updated successfully');
+            },
+            error: function (error) {
+                console.error('Error updating profile information');
+            }
+        });
+    }
 });
