bc6c3782587866c5cf4076918268c27269d62a43
Nic <nicolecadman@icloud.com>
2023-12-30 13:47:12 +0000

    Refactor: Change fname and lname to name in views.py

--- contact/views.py
+++ contact/views.py
@@ -9,7 +9,6 @@ def contact_us(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            print("Form is valid!") 
             contact_submission = ContactSubmission(
                 name=form.cleaned_data['name'],
                 email=form.cleaned_data['email'],
@@ -19,17 +18,18 @@ def contact_us(request):
             contact_submission.save()  # Save the data to the database
 
             send_mail(
-                form.cleaned_data['subject'],  # subject
-                f"Message from {form.cleaned_data['fname']} {form.cleaned_data['lname']} "
+                form.cleaned_data['subject'], 
+                f"Message from {form.cleaned_data['name']} "
                 f"<{form.cleaned_data['email']}>\n\n"
-                f"{form.cleaned_data['message']}",  # message
-                None,  # from email
-                ['contact@southvillesportingclub.com'],
+                f"{form.cleaned_data['message']}",  
+                'tomedi6527@usoplay.com',
+                ['tomedi6527@usoplay.com'],
             )
+
             messages.success(
                 request,
                 'Thank you for submitting! We will be in touch asap!')
-            return render(request, 'contact/contact_us.html')
+            return render(request, 'home/index.html')
 
     else:
         form = ContactForm()
