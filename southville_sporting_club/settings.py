67f86779807e7ea92c75dcc82984aa8e95e8bd59
Nic <nicolecadman@icloud.com>
2024-01-02 13:13:48 +0000

    Removed debug from settings

--- southville_sporting_club/settings.py
+++ southville_sporting_club/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'southville-sporting-club-ed7d11c9dd6e.herokuapp.com', '8000-nccadman19-southvillesp-xmp9b25vp00.ws-eu107.gitpod.io']
 
